de38eac95f5a64d0b837835aa511d28580be6493
ANUSHA REHMAN QURESHI <anusharehmanqureshi@gmail.com>
2025-07-30 15:17:54 +0500

    Update app.py

--- agentanti/app.py
+++ agentanti/app.py
@@ -105,7 +105,7 @@ st.markdown("""
     <div class="banner">
         <img src='https://tse1.mm.bing.net/th/id/OIP.XSegwM_A-Vy6pAGa6GMP8gHaK_?r=0&rs=1&pid=ImgDetMain&o=7&rm=3' alt="Marriage Bureau Banner" />
     </div>
-    <div class="center-title">💍Alishba Marriage Bureau💍 </div>
+    <div class="center-title">💍Marriage Bureau💍 </div>
 """, unsafe_allow_html=True)
 
 rain(emoji="❤️", font_size=20, falling_speed=5, animation_length="infinite")
